a131e5f25cc06a87b2a92b2a6f3cdbf9117b361c
Shaa42 <ryanvigoureux@hotmail.com>
2026-01-21 09:05:11 +0100

    OTHER: New output color

--- color.py
+++ color.py
@@ -3,8 +3,9 @@ def colorString(color="", string=""):
     GREEN = "\033[32m"
     YELLOW = "\033[33m"
     BLUE = "\033[34m"
-    PURPLE = "\033[35m"
+    MAGENTA = "\033[35m"
     CYAN = "\033[36m"
+    BRIGHT_MAGENTA = "\033[95m"
     ENDC = "\033[0m"
 
     match color:
@@ -16,9 +17,11 @@ def colorString(color="", string=""):
             return f"{YELLOW}{string}{ENDC}"
         case "blue":
             return f"{BLUE}{string}{ENDC}"
-        case "purple":
-            return f"{PURPLE}{string}{ENDC}"
+        case "magenta":
+            return f"{MAGENTA}{string}{ENDC}"
         case "cyan":
             return f"{CYAN}{string}{ENDC}"
+        case "bright_magenta":
+            return f"{BRIGHT_MAGENTA}{string}{ENDC}"
         case _:
             return string
